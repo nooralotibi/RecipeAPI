8c7f603146c0f8683fb2c530d65db3e0e01ec84a
nooreealotibi <nooreealotibi@gmail.com>
2024-02-11 23:47:33 +0300

    Initial Commit

---
+++ RecipeAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecipeAPI
+//
+//  Created by noor alotibi on 01/08/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecipeAPI/RecipeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAPIApp.swift
+//  RecipeAPI
+//
+//  Created by noor alotibi on 01/08/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
